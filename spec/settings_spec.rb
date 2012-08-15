0f200f01d938dd0e92c0aeee7481898c1366bfd0
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-08-14 19:14:17 -0700

    more unit tests

--- spec/settings_spec.rb
+++ spec/settings_spec.rb
@@ -17,83 +17,106 @@ class Ruhoh
           Api
         end
 
-        it 'should return site.yml as yaml' do
-          get '/settings/site', {}, {'HTTP_ACCEPT' => 'application/x-yaml'}
-          last_response.should be_ok
-          last_response.content_type.should match %r{^application/x-yaml}
-          last_response.body.should == File.open(SITE_YML){ |f| f.read }
+        context 'well-formed requests' do
+
+          it 'should return site.yml as yaml' do
+            get '/settings/site', {}, {'HTTP_ACCEPT' => 'application/x-yaml'}
+            last_response.should be_ok
+            last_response.content_type.should match %r{^application/x-yaml}
+            last_response.body.should == File.open(SITE_YML){ |f| f.read }
+          end
+
+          it 'should return site.yml as json' do
+            get '/settings/site', {}, {'HTTP_ACCEPT' => 'application/json'}
+            last_response.should be_ok
+            last_response.content_type.should match %r{^application/json}
+            yaml = File.open(SITE_YML) { |f| f.read }
+            last_response.body.should == YAML.load(yaml).to_json
+          end
+
+          it 'should return site.yml as text' do
+            get '/settings/site', {}, {'HTTP_ACCEPT' => 'text/plain'}
+            last_response.should be_ok
+            last_response.content_type.should match %r{^text/plain}
+            last_response.body.should == File.open(SITE_YML) { |f| f.read }
+          end
+
+          it 'should return config.yml as yaml' do
+            get '/settings/config', {}, {'HTTP_ACCEPT' => 'application/x-yaml'}
+            last_response.should be_ok
+            last_response.content_type.should match %r{^application/x-yaml}
+            last_response.body.should == File.open(CONFIG_YML) { |f| f.read }
+          end
+
+          it 'should return config.yml as json' do
+            get '/settings/config', {}, {'HTTP_ACCEPT' => 'application/json'}
+            last_response.should be_ok
+            last_response.content_type.should match %r{^application/json}
+            yaml = File.open(CONFIG_YML) { |f| f.read }
+            last_response.body.should == YAML.load(yaml).to_json
+          end
+
+          it 'should return config.yml as text' do
+            get '/settings/config', {}, {'HTTP_ACCEPT' => 'application/x-yaml'}
+            last_response.should be_ok
+            last_response.content_type.should match %r{^application/x-yaml}
+            last_response.body.should == File.open(CONFIG_YML) { |f| f.read }
+          end
+
+          it 'should return payload as json' do
+            get '/settings/payload', {}, {'HTTP_ACCEPT' => 'application/json'}
+            last_response.should be_ok
+            last_response.content_type.should match %r{^application/json}
+            last_response.body.should == Ruhoh::DB.payload.to_json
+          end
+
+          it 'should return payload as text' do
+            get '/settings/payload', {}, {'HTTP_ACCEPT' => 'text/plain'}
+            last_response.should be_ok
+            last_response.content_type.should match %r{^text/plain}
+            last_response.body.should == Ruhoh::DB.payload.pretty_inspect
+          end
+
+          it 'should return payload as yml' do
+            get '/settings/payload', {}, {'HTTP_ACCEPT' => 'application/x-yaml'}
+            last_response.should be_ok
+            last_response.content_type.should match %r{^application/x-yaml}
+            last_response.body.should == Ruhoh::DB.payload.to_yaml
+          end
+
+          it 'should overwrite site.yml' do
+            put '/settings/site', 'some 无聊的 UTF-8 text'
+            last_response.should be_ok
+            contents = File.open(SITE_YML, 'r:UTF-8') { |f| f.read }
+            contents.should == "some 无聊的 UTF-8 text"
+          end
+
+          it 'should overwrite config.yml' do
+            put '/settings/config', 'some 无聊的 UTF-8 text'
+            last_response.should be_ok
+            contents = File.open(CONFIG_YML, 'r:UTF-8') { |f| f.read }
+            contents.should == "some 无聊的 UTF-8 text"
+          end
+
         end
 
-        it 'should return site.yml as json' do
-          get '/settings/site', {}, {'HTTP_ACCEPT' => 'application/json'}
-          last_response.should be_ok
-          last_response.content_type.should match %r{^application/json}
-          yaml = File.open(SITE_YML) { |f| f.read }
-          last_response.body.should == YAML.load(yaml).to_json
-        end
+        context 'malformed requests' do
 
-        it 'should return site.yml as text' do
-          get '/settings/site', {}, {'HTTP_ACCEPT' => 'text/plain'}
-          last_response.should be_ok
-          last_response.content_type.should match %r{^text/plain}
-          last_response.body.should == File.open(SITE_YML) { |f| f.read }
-        end
+          it 'should not deny access to private methods' do
+            get '/settings/get', {}, {'HTTP_ACCEPT' => 'application/x-yaml'}
+            last_response.should be_not_found
+          end
 
-        it 'should return config.yml as yaml' do
-          get '/settings/config', {}, {'HTTP_ACCEPT' => 'application/x-yaml'}
-          last_response.should be_ok
-          last_response.content_type.should match %r{^application/x-yaml}
-          last_response.body.should == File.open(CONFIG_YML) { |f| f.read }
-        end
+          it 'should never allow access to other files' do
+            api = mock app
+            req = Rack::MockRequest.new api
+            api.should_receive(:error).and_throw(:halt)
+            controller = SettingsController.new api, req
+            expect {
+              controller.send :get, 'x', ['application/json']
+            }.to throw_symbol :halt
+          end
 
-        it 'should return config.yml as json' do
-          get '/settings/config', {}, {'HTTP_ACCEPT' => 'application/json'}
-          last_response.should be_ok
-          last_response.content_type.should match %r{^application/json}
-          yaml = File.open(CONFIG_YML) { |f| f.read }
-          last_response.body.should == YAML.load(yaml).to_json
-        end
-
-        it 'should return config.yml as text' do
-          get '/settings/config', {}, {'HTTP_ACCEPT' => 'application/x-yaml'}
-          last_response.should be_ok
-          last_response.content_type.should match %r{^application/x-yaml}
-          last_response.body.should == File.open(CONFIG_YML) { |f| f.read }
-        end
-
-        it 'should return payload as json' do
-          get '/settings/payload', {}, {'HTTP_ACCEPT' => 'application/json'}
-          last_response.should be_ok
-          last_response.content_type.should match %r{^application/json}
-          last_response.body.should == Ruhoh::DB.payload.to_json
-        end
-
-        it 'should return payload as text' do
-          get '/settings/payload', {}, {'HTTP_ACCEPT' => 'text/plain'}
-          last_response.should be_ok
-          last_response.content_type.should match %r{^text/plain}
-          last_response.body.should == Ruhoh::DB.payload.pretty_inspect
-        end
-
-        it 'should return payload as yml' do
-          get '/settings/payload', {}, {'HTTP_ACCEPT' => 'application/x-yaml'}
-          last_response.should be_ok
-          last_response.content_type.should match %r{^application/x-yaml}
-          last_response.body.should == Ruhoh::DB.payload.to_yaml
-        end
-
-        it 'should overwrite site.yml' do
-          put '/settings/site', 'some 无聊的 UTF-8 text'
-          last_response.should be_ok
-          contents = File.open(SITE_YML, 'r:UTF-8') { |f| f.read }
-          contents.should == "some 无聊的 UTF-8 text"
-        end
-
-        it 'should overwrite config.yml' do
-          put '/settings/config', 'some 无聊的 UTF-8 text'
-          last_response.should be_ok
-          contents = File.open(CONFIG_YML, 'r:UTF-8') { |f| f.read }
-          contents.should == "some 无聊的 UTF-8 text"
         end
 
       end

ebbddc0b8830e3665634c150f8815b3cdb7cd710
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-08-15 19:05:36 -0700

    added controller actions and tests for pagescontroller

--- lib/ruhoh-manager/controllers/pages_controller.rb
+++ lib/ruhoh-manager/controllers/pages_controller.rb
@@ -1,9 +1,22 @@
 class Ruhoh
   module Manager
 
-    # Controls pages actions
+    # Controls access to pages.
     # @author Jim Lim
-    class PagesController < ApplicationController
+    class PagesController < FilesController
+
+      protected
+
+      def resolve_uri(uri)
+        # TODO: multiple ruhohs
+        return File.expand_path uri, Ruhoh.paths.pages
+      end
+
+      def is_allowed?(path)
+        # TODO: multiple ruhohs
+        File.fnmatch File.join(Ruhoh.paths.pages, '**'), path
+      end
+
     end
 
   end
---
+++ spec/pages_spec.rb
@@ -0,0 +1,103 @@
+# encoding: utf-8
+require 'spec_helper'
+
+class Ruhoh
+  module Manager
+    module PagesSpec
+
+      describe 'Pages Controller' do
+
+        include Rack::Test::Methods
+
+        Names = OpenStruct.new(Ruhoh::Names)
+        PAGES_DIR = File.join(TEMP_SITE_PATH, Names.pages)
+
+        def app
+          Api
+        end
+
+        context 'well-formed requests' do
+
+          it 'should return pages as text' do
+            f = File.join PAGES_DIR, 'hello_test.md'
+            IO.write f, 'ただいま', :mode => 'wb+'
+            get '/pages/hello_test.md'
+            last_response.should be_ok
+            last_response.content_type.should match %{^text/plain;charset=utf-8}
+            last_response.body.force_encoding('utf-8').should == 'ただいま'
+          end
+
+          it 'should handle pages within directories' do
+            f = File.join(PAGES_DIR, 'some', 'nested', 'structure', 'x.md')
+            FileUtils.mkdir_p File.dirname(f)
+            IO.write f, '«»≈≠Ωº', :mode => 'wb+'
+            get '/pages/some/nested/structure/x.md'
+            last_response.should be_ok
+            last_response.content_type.should match %{^text/plain;charset=utf-8}
+            last_response.body.force_encoding('utf-8').should == '«»≈≠Ωº'
+          end
+
+          it 'should write successfully and return a 200 if file exists' do
+            target = File.join(PAGES_DIR, 'target.md')
+            IO.write target, 'here is some english text', :mode => 'wb+'
+            put '/pages/target.md', '柳暗花明又一村'
+            last_response.should be_ok
+            IO.read(target).should == '柳暗花明又一村'
+          end
+
+          it 'should write successfully and return a 201 if file was created' do
+            target = File.join(PAGES_DIR, 'target.md')
+            put '/pages/target.md', '山重水复疑无路'
+            last_response.status.should == 201
+            IO.read(target).should == '山重水复疑无路'
+          end
+
+          it 'should delete the specified page if it exists' do
+            target = File.join(PAGES_DIR, 'target.md')
+            IO.write target, 'hello there this is jim', :mode => 'wb+'
+            File.file?(target).should be_true
+            delete '/pages/target.md'
+            last_response.should be_ok
+            File.file?(target).should be_false
+          end
+
+        end
+
+        context 'malformed requests' do
+          it 'should return 403 if resource path is not under +pages+' do
+            # TODO: both get and put
+          end
+        end
+
+        context 'reading/writing to other files' do
+
+          it 'should return a 404 if getting unknown resource' do
+            get '/pages/xyz'
+            last_response.should be_not_found
+          end
+
+          it 'should return a 409 if the target path is a directory' do
+            target = File.join(PAGES_DIR, 'target')
+            FileUtils.mkdir_p target
+            put '/pages/target', 'some content'
+            last_response.status.should == 409
+          end
+
+          it 'should return a 404 if deleting unknown page' do
+            delete '/pages/x'
+            last_response.status.should == 404
+          end
+
+          it 'should return 403 if deleting directory' do
+            FileUtils.mkdir File.join(PAGES_DIR, 'x')
+            delete '/pages/x'
+            last_response.status.should == 403
+          end
+
+        end
+
+      end
+
+    end
+  end
+end

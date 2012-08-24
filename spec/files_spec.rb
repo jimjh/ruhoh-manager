a9b58aa0645e11b1e1c29c3fded893c201e50db0
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-08-24 15:40:20 -0400

    added unit test for hidden files

---
+++ spec/files_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+class Ruhoh
+  module Manager
+    module FilesSpec
+
+      # Tests generic functions shared across pages/posts/partials/media
+      describe 'Files Controller' do
+
+        include Rack::Test::Methods
+
+        def app
+          Api
+        end
+
+        it 'should deny access to hidden files' do
+          api = mock app
+          api.should_receive(:error).and_throw(:halt)
+          controller = FilesController.new api
+          expect {
+            controller.send :_get, '.x', ['application/json']
+          }.to throw_symbol :halt
+        end
+
+      end
+
+    end
+  end
+end

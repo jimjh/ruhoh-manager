e9b27901465c1cd235b0b2b1b746a2aeb6239eaa
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-08-24 23:18:26 -0400

    added comments

--- lib/ruhoh-manager/accessories/status_code.rb
+++ lib/ruhoh-manager/accessories/status_code.rb
@@ -6,26 +6,27 @@ class Ruhoh
     # it requires method delegation to a Sinatra app.
     module Accessory::StatusCode
 
-      # Set status to Forbidden and stop
+      # Set status to Forbidden and stop.
       def forbidden(body = nil)
         json_halt Rack::Utils.status_code(:forbidden), body
       end
 
-      # Set status to Bad Request and stop
+      # Set status to Bad Request and stop.
       def bad_request(body = nil)
         json_halt Rack::Utils.status_code(:bad_request), body
       end
 
-      # Set status to Conflict and stop
+      # Set status to Conflict and stop.
       def conflict(body = nil)
         json_halt Rack::Utils.status_code(:conflict), body
       end
 
+      # Set status to Not Found and stop.
       def not_found(body = nil)
         json_halt Rack::Utils.status_code(:not_found), body
       end
 
-      # Set status to Internal Server Error and stop
+      # Set status to Internal Server Error and stop.
       def internal_server_error(body = nil)
         json_halt Rack::Utils.status_code(:internal_server_error), body
       end
@@ -44,7 +45,7 @@ class Ruhoh
       # @param [Numeric] code           status code
       # @param [String] message         message body
       # Example:
-      #   json_halt 404, "Not found" # >> {"message": "Not found."}
+      #   json_halt 404, "Not found" #>> {"message": "Not found."}
       def json_halt(code, message)
         halt code,
              {'Content-Type' => mime_type(:json)},
